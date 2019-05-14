18fe509321caf0e6fa30db1e7f7d56af58b0d0a8
earyzhe <earyzhe@gmail.com>
2019-05-14 07:36:54 +0200

    The data is going to the serever for the canvas

--- public/script.js
+++ public/script.js
@@ -5,32 +5,6 @@
     context.strokeStyle = '#900'
     context.lineWidth = 2
 
-    // $('#signature').mousedown(function (event) {
-    //     context.beginPath()
-    //     // var x = (event.clientX - this.offsetLeft) * this.width / this.clientWidth
-    //     // var y = (event.clientY - this.offsetTop) * this.height / this.clientHeight
-    //     // var x = event.clientX - $('#signature').offset().left
-    //     // var y = event.clientY - $('#signature').offset().top
-    //     var x = event.offsetX
-    //     var y = event.offsetY
-    //     console.log(event.offsetX, event.offsetY)
-    //     console.log(x, y)
-
-    //     context.moveTo(x, y)
-
-    //     $('#signature').mousemove(function (event) {
-    //         var x = (event.clientX - this.offsetLeft) * this.width / this.clientWidth
-    //         var y = (event.clientY - this.offsetTop) * this.height / this.clientHeight
-    //         context.lineTo(x, y)
-    //         console.log(x, y)
-    //         context.stroke()
-    //     }).on('mouseup mouseleave', function (event) { $('#signature').off('mousemove mouseleave') })
-    // })
-
-    // $('#sumbit').mousedown(function (e) {
-    //     $('"input[name="signature]"').val($('#signature').toDataURL())
-    // })
-
     // Kinda working
     $('#signature').mousedown(function (event) {
         context.beginPath()
@@ -56,10 +30,9 @@
 
             context.lineTo(x, y)
             context.stroke()
+            console.log(this);
+            var data = this.toDataURL()
+            $('#data').val(data)
         })
     }).on('mouseup mouseleave', function (event) { $('#signature').off('mousemove mouseleave') })
-
-    $('#sumbit').mousedown(function (e) {
-        $('"input[name="signature]"').val($('#signature').toDataURL())
-    })
 })()
